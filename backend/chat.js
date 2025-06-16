ed2c29c5091f2937fa7adc43fb8d825f28182f42
Nakorn Boonprasong <curseboyz@gmail.com>
2025-06-16 14:16:46 +0700

    Updated chat.js to prompt OpenAI model

--- backend/chat.js
+++ backend/chat.js
@@ -98,19 +98,15 @@ app.post("/prompt", async (req, res) => {
     // console.log(req.body.messages[0].content) 
     // console.log(Object.keys(req)) 
 
-    const response = await fetch("http://localhost:1234/v1/chat/completions", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          model: "bartowski/Qwen2.5-32B-Instruct-GGUF",
-          messages: [
-            {
-                role: "system",
-                content: prompt_msg,
-            },
-          ],
-        }),
-      });
+    const completion = await client.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: [
+        {
+          role: role,
+          content: prompt_msg,
+        },
+      ],
+    });
 
     const data = await response.json();
     // console.log(data.choices[0].message.content)
